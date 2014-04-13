582a3fb1310ebae581ca37895b50b50517804008
Pronin Alexander <pronin.alx@gmail.com>
2014-04-13 19:13:23 +0300

    Initial Commit

---
+++ 2403DrawingsTestHW/RITViewController.h
@@ -0,0 +1,13 @@
+//
+//  RITViewController.h
+//  2403DrawingsTestHW
+//
+//  Created by Aleksandr Pronin on 13.04.14.
+//  Copyright (c) 2014 Aleksandr Pronin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RITViewController : UIViewController
+
+@end
---
+++ 2403DrawingsTestHW/RITViewController.m
@@ -0,0 +1,29 @@
+//
+//  RITViewController.m
+//  2403DrawingsTestHW
+//
+//  Created by Aleksandr Pronin on 13.04.14.
+//  Copyright (c) 2014 Aleksandr Pronin. All rights reserved.
+//
+
+#import "RITViewController.h"
+
+@interface RITViewController ()
+
+@end
+
+@implementation RITViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
